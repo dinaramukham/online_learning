484f0d64d2dbabe82975ca319aa2f5da777e7b86
Dinara Mukhametzianova <muxametmdm@gmail.com>
2024-01-26 01:20:17 +0500

    модели без миграций

--- online_learning/users/models.py
+++ online_learning/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone=models.IntegerField()
+    city=models.CharField(max_length=50 )
+    avatar=models.ImageField(upload_to='media/avatar/')
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
