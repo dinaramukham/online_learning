306034a5015401cc36245185c209fba1ca4cad34
Dinara Mukhametzianova <muxametmdm@gmail.com>
2024-02-15 16:56:38 +0500

    models

--- online_learning/courses/models.py
+++ online_learning/courses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class Courses(models.Model  ):
+    title=models.CharField(max_length=100 )
+    photo=models.ImageField(upload_to='media/photo/')
+    content=models.TextField()
+    def __str__(self):
+        return self.title
+class Lesson(models.Model):
+    courses=models.ForeignKey(Courses, on_delete=models.DO_NOTHING, null=True, blank=True  )
+    title = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='media/photo/')
+    content = models.TextField()
+    link_video=models.TextField()
+    def __str__(self):
+        return self.title
