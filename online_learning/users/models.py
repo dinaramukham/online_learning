b195f6bd5a3b08b65eb9d13ccffa861fd5e38ea4
Dinara Mukhametzianova <muxametmdm@gmail.com>
2024-02-17 15:17:18 +0500

    миграция courses

--- online_learning/users/models.py
+++ online_learning/users/models.py
@@ -1,3 +1,31 @@
+from django.utils import timezone
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+from courses.models import Courses, Lesson
+
+
 # Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length= 35)
+    city = models.CharField(max_length=50, null=True, blank= True,)
+    avatar = models.ImageField(null=True,  blank= True,  upload_to='media/avatar/')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    def __str__(self):
+        return self.email
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    courses = models.OneToOneField(Courses, on_delete=models.CASCADE, null=True, blank=True, primary_key= True)
+    lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE, null=True, blank=True, primary_key= True)
+    method_pay=models.CharField(max_length= 15, choices= (('cash', 'наличными'), ('card', 'картой')))
+    date_payment = models.DateField(default= timezone.now)
+    money = models.IntegerField()
+    def __str__(self):
+        return self.user
