5e92c5d4a1586eb905d9bb18bd810ad7c3646cd2
Dinara Mukhametzianova <muxametmdm@gmail.com>
2024-02-18 22:28:09 +0500

    миграции

--- online_learning/courses/models.py
+++ online_learning/courses/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 class Courses(models.Model  ):
     title=models.CharField(max_length=100 )
-    photo=models.ImageField(upload_to='media/photo/')
+    photo=models.ImageField(upload_to='media/photo/', null=True, blank= True,)
     content=models.TextField()
     def __str__(self):
         return self.title
 class Lesson(models.Model):
     courses=models.ForeignKey(Courses, on_delete=models.DO_NOTHING, null=True, blank=True  )
     title = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='media/photo/')
+    photo = models.ImageField(upload_to='media/photo/', null=True, blank= True,)
     content = models.TextField()
-    link_video=models.TextField()
+    link_video=models.TextField(null=True, blank= True,)
     def __str__(self):
         return self.title
--- online_learning/users/models.py
+++ online_learning/users/models.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
@@ -10,10 +10,20 @@ from django.db import models
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length= 35)
+    phone = models.CharField(max_length= 35, null=True, blank= True,)
     city = models.CharField(max_length=50, null=True, blank= True,)
     avatar = models.ImageField(null=True,  blank= True,  upload_to='media/avatar/')
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",
+        blank=True,
+    )
 
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_set",
+        blank=True,
+    )
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     def __str__(self):
