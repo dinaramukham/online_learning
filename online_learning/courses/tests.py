567c2c53ae0740aa86cd7b7463581b15a4ad154a
Dinara Mukhametzianova <muxametmdm@gmail.com>
2024-02-26 00:08:22 +0500

    тесты

--- online_learning/courses/tests.py
+++ online_learning/courses/tests.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from online_learning.courses.models import Courses, Lesson
@@ -9,20 +9,67 @@ from online_learning.users.models import User
 # Create your tests here.
 class CoursesTest(APITestCase):
     def setUp(self):
-        self.user = User.objects.create(email='teatuser@gmail.ru')
+        self.client = APIClient()
+        self.user = User.objects.create(email='teatuser@gmail.ru', is_superuser=True, is_staff=True)
         self.user.set_password('test')
-        self.user.save()
-        #access_token = str(RefreshToken.for_user(self.user).access_token)
-        #self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token }')
-        self.courses=Courses.objects.create(title='test', content='test')
-        self.courses.save()
-        self.lesson = Lesson.objects.create(courses=Courses.objects.get(title='test'), title="test", content="test", link_video="https://www.youtube.com/test")
+
+        self.client.force_authenticate(user=self.user)
+
     def test_create_courses(self):
         data = {'tittle': 'Test', 'content': 'test'}
         response = self.client.post('courses/create/', data=data)
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
     def test_list_courses(self):
-
         response = self.client.get('courses/list/')
-        self.assertEquals(response.status_code, status.HTTP_200_OK,)
+        self.assertEquals(response.status_code, status.HTTP_200_OK, )
+
+    def test_destroy_courses(self):
+        course = Courses.objects.create(user=self.user, title='test course', content='test content')
+        # course.save()
+        response = self.client.delete(f'courses/delete/{course.id}/')
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_update_courses(self):
+        course = Courses.objects.create(user=self.user, title='test course', content='test content')
+        new_title = "new test"
+        response = self.client.patch(f'/lessons/update/{course.id}/', data={"new_title": new_title}, )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class LessonTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create(email='teatuser@gmail.ru', is_superuser=True, is_staff=True)
+        self.user.set_password('test')
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_lesson(self):
+        data = {"title": "test", "content": "test", }
+        response = self.client.post('courses/create/', data=data)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list_lesson(self):
+        response = self.client.get('courses/list/')
+        self.assertEquals(response.status_code, status.HTTP_200_OK, )
+
+    def test_destroy_lesson(self):
+        course = Lesson.objects.create(user=self.user,
+                                       courses=Courses.objects.get(pk=1),
+                                       title='test course',
+                                       content='test content', link_video="")
+        # course.save()
+        response = self.client.delete(f'courses/delete/{course.id}/')
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_update_lesson(self):
+        course = Lesson.objects.create(user=self.user,
+                                       courses=Courses.objects.get(pk=1),
+                                       title='test course',
+                                       content='test content', link_video="")
+        new_title = "new test"
+        response = self.client.patch(f'/lessons/update/{course.id}/',
+                                     data={"new_title": new_title}, )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
